c113f79fe62769674eac398e6417ceb8d3ede04a
Nadhrah Fuad <nadhrah.fuad@gmail.com>
2023-07-31 13:42:35 +0800

    upload

---
+++ vuepractice-main copy/shopeenew/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- <div>
+    <router-link :to="{ name: 'SplashScreen' }">Splash</router-link> |
+
+  </div> -->
+
+  <!-- <router-view /> -->
+  <SplashScreenPage/>
+  <SignInPage /> 
+  <SignUpPage/>
+  <ForgotPasswordPage/>
+  <WelcomeBackPage/>
+</template>
+
+<script setup>
+// import router from './router';
+
+// import CategorySlider from '@/components/sections/CategorySlider.vue'
+// import CategoriesSection from '@/components/sections/CategoriesSection.vue'
+
+// import HomePage from './pages/public/HomePage.vue'
+//import SplashScreenPage from './pages/public/SplashScreenPage.vue'
+//import SignInPage from './pages/public/SignInPage.vue'
+//import SignUpPage from './pages/public/SignUpPage.vue'
+//import ForgotPasswordPage from './pages/public/ForgotPasswordPage.vue'
+//import RecoveryPage from './pages/public/RecoveryPage.vue'
+//import NewPasswordPage from './pages/public/NewPasswordPage.vue'
+// import PwResetSuccessPage from './pages/public/PwResetSuccessPage.vue'
+import WelcomeBackPage from './pages/public/WelcomeBackPage.vue'
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 850px;
+  background-color: #d0cccc;
+}
+
+.margin {
+  margin-left: 10px;
+}
+</style>
